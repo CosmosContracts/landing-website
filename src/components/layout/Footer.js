959a3c300528fc2b974d0d4b133bd6c30e7b0020
Block <cosmoscontracts@protonmail.com>
2021-03-23 15:53:11 +0100

    Changed domain link and meta desc
    Changed domain link and meta desc

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -45,7 +45,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by <a href="https://domain.com">Juno</a>. All right reserved</div>
+            <div className="footer-copyright">Made by <a href="https://junochain.com">Juno</a>. All right reserved</div>
           </div>
         </div>
       </div>
